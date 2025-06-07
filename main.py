12b5b31731a63000ae90a0d2a703af23d95562ba
leandrodiascarvalho <leandrovinte100@gmail.com>
2025-06-07 14:54:47 -0300

    cria funcao sacar e extrato

--- main.py
+++ main.py
@@ -25,3 +25,27 @@ def depositar(saldo, extrato, /, valor):
         print("Operação falhou! O valor informado é inválido.")
     return saldo, extrato
 
+def sacar(*, saldo, extrato, valor, limite, numero_saques, limite_saques):
+    if valor > saldo:
+        print("Não será possível realizar o saque por saldo insuficiente.")
+    elif valor > limite:
+        print("O valor do saque excede o limite.")
+    elif numero_saques >= limite_saques:
+        print("Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato.append(f"Saque: R$ {valor:.2f}")
+        numero_saques += 1
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato, numero_saques
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("\n========== EXTRATO ==========")
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+    else:
+        for operacao in extrato:
+            print(operacao)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==============================")
